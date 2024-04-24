ac6b9d5979b94c87df3d876c9e1288c90f02448f
JPregliasco <91738384+juanpi8345@users.noreply.github.com>
2024-04-24 15:35:22 -0300

    Delete Practicas java directory

--- Practicas java/junit5/src/main/java/com/mockito/junit5/service/IServiceA.java
+++
@@ -1,5 +0,0 @@
-package com.mockito.junit5.service;
-
-public interface IServiceA {
-    int add(int a, int b);
-}
--- Practicas java/testingProject/src/main/java/com/junitMockito/testingProject/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.junitMockito.testingProject.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
